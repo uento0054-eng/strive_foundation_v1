1d2af4fc92a376f02ab288bf6f6fd4613e8e16d1
uento0054-eng <uento0054@gmail.com>
2026-03-01 13:31:18 +0530

    Add files via upload

---
+++ lib/features/exercises/exercises_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ExercisesScreen extends StatelessWidget {
+  const ExercisesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Exercises")),
+      body: const Center(child: Text("Exercises Module Coming Next")),
+    );
+  }
+}
